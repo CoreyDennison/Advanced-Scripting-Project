009958f0ac74ca9386ac9fa29e6266780e3ea169
Corey Dennison <Corey.Dennison@students.ittralee.ie>
2023-03-29 16:37:51 -0400

    Create own basic view with <h1> tag.

--- as_django/views.py
+++ as_django/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def student_attend_view(*args, **kwargs):
+    return HttpResponse("<h2>Student Attendance App</h2>")
--- as_project/urls.py
+++ as_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from as_django import views
 
 urlpatterns = [
+    path('', views.student_attend_view, name='home'),
     path("admin/", admin.site.urls),
 ]
