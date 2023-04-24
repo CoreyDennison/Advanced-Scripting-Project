2562e3f14c9079dbac5465c87cec59d46af17cfb
Corey Dennison <Corey.Dennison@students.ittralee.ie>
2023-04-24 22:03:50 +0100

    Created "POST" method for form.
    Created "POST" method for form to add values of checkboxes to attendance table. However, I receive a "Data too long for row - " error.

--- as_django/views.py
+++ as_django/views.py
@@ -11,4 +11,22 @@ def index_view(request):
         'students': student_table,
         'attendance': attendance_table,
     }
+
+    if request.method == "POST":
+        John_is_present = request.POST.get('John_is_present', 'n')
+        Michael_is_present = request.POST.get('Michael_is_present', 'n')
+        Beth_is_present = request.POST.get('Beth_is_present', 'n')
+        Nick_is_present = request.POST.get('Nick_is_present', 'n')
+        Mark_is_present = request.POST.get('Mark_is_present', 'n')
+
+        attendance_entry = attendance(
+            student1_present = John_is_present,
+            student2_present = Michael_is_present,
+            student3_present = Beth_is_present,
+            student4_present = Nick_is_present,
+            student5_present = Mark_is_present
+        )
+        attendance_entry.save()
+
+
     return HttpResponse(template.render(context,request))
